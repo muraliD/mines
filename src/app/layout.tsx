3e56f86ecbe561c7d8fe899cadd80bbd830bc22a
murali <murali.dadi11@gmail.com>
2024-03-16 20:27:00 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from "@mui/material";
-import theme from "@/theme";
+// import theme from "@/theme";
+import theme from "../theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/pages/base/page.tsx
+++ src/app/pages/base/page.tsx
@@ -1,14 +1,68 @@
-import React from "react";
-import { Button } from "@mui/material";
-const BasePage = () =>{
-    return (
-    <div >
-<Button variant="text">Text</Button>
-<Button variant="contained">Contained</Button>
-<Button variant="outlined">Outlined</Button>
+"use client"
+import React,{useEffect, useState} from 'react';
+import $ from 'jquery'
+import Image from 'next/image';
+import ocean from "../../../../public/images/res.jpg";
+import sea from '../../../../public/images/res.jpg';
+import woman from '../../../../public/images/res.jpg';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import dynamic from 'next/dynamic';
 
+const OwlCarousel = dynamic(() => import('react-owl-carousel'), {
+  ssr: false,
+});
+
+const options = {
+  loop: false,
+  margin: 10,
+  items: 1,
+  autoplay: false,
+};
+
+export default function Basepage() {
+
+    const [windows,setwindows] = useState<any>(null)
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setwindows(window);
+            window.$ = window.jQuery = $
+        }
+      }, [typeof window ]);
+  return (
+    <div>
+     {windows && 
+     <div>
+      <OwlCarousel className="owl-theme" {...options}>
+        <div className="item">
+          <Image
+            src={ocean}
+            alt="Ocean"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="item">
+          <Image
+            src={sea}
+            alt="Sea"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="item">
+          <Image
+            src={woman}
+            alt="Woman"
+            width={100}
+            height={100}
+          />
+        </div>
+      </OwlCarousel>
+      </div>
+      
+      }
     </div>
-    )
-
-}  
-export default  BasePage
+  );
+}
